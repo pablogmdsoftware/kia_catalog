6cb421811656130eec81b4bdd985b99ac66a189e
PabloGMD <pablogmdsoftware@gmail.com>
2025-11-18 12:39:37 +0100

    feature: Add click_load_buttons function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
-from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from urls import URLS
+import time
 
 class GetDriver():
     def __init__(self):
@@ -9,7 +14,20 @@ class GetDriver():
     def __exit__(self,exc_type,exc_value,exc_traceback):
         self.driver.close()
 
+def click_load_buttons(driver):
+    wait = WebDriverWait(driver, 10)
+    while True:
+        try:
+            button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'EH1xtIwTxIU-')]")))
+            driver.execute_script("arguments[0].scrollIntoView(true);", button)
+            time.sleep(0.2)
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(0.7)
+        except TimeoutException:
+            print("There are no more buttons to click.")
+            break
+
 with GetDriver() as driver:
-    driver.get("https://www.parts-catalogs.com/eu/demo#/catalogs?catalogId=kia")
-    sleep(1)
+    driver.get("https://www.parts-catalogs.com/eu/demo#/models?catalogId=kia&modelId=1d8274a18f0139854e588412541067fb")
+    click_load_buttons(driver)
 
